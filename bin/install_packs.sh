c3b05e42eacc8bad4dbb33fd27eb6ba61c089829
Dylan Curzon <dylan@DYLAN-PC.localdomain>
2022-06-21 13:24:17 +0100

    Add install packages script for vim

---
+++ bin/install_packs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#install vim-packs via https://github.com/mg979/vim-packs
+vpacks install dag/vim-fish
+vpacks install mg979/vim-visual-multi
+vpacks install itchyny/lightline.vim
+vpacks install sainnhe/gruvbox-material
